70e8c1aeb9f179805990313ebfcd3dffbd247acf
Ebs12345678 <subhransuseo18@gmail.com>
2024-03-20 19:40:46 +0530

    new changes 18032024-11

--- src/app/Ourteam/OurTeam.js
+++ src/app/Ourteam/OurTeam.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import jamelimg from "../../../public/images/team/jamel.jpeg";
-import steveimg from "../../../public/images/team/ahn.jpeg";
-import simonimg from "../../../public/images/team/simon.jpeg";
-import hanimg from "../../../public/images/team/han.jpeg";
+import jamelimg from "../../../public/images/team/jamel.png";
+import steveimg from "../../../public/images/team/ahn.png";
+import simonimg from "../../../public/images/team/simon.png";
+import hanimg from "../../../public/images/team/han.png";
 const OurTeam = () => {
     return (
         <>
@@ -16,7 +16,7 @@ const OurTeam = () => {
 
                             </div>
                         </div>
-                        <div className="col-md-6 col-lg-8 col-xl-8 col-sm-12">
+                        <div className="col-md-6 col-lg-8 col-xl-8 col-sm-12 team">
                             <h4>Jamiel Jamieson</h4>
                             <p>Jamiel Jamieson is a seasoned professional with over 15 years of experience in commodity trading across various industries. His expertise spans a wide range of products, from agricultural goods to precious metals. Throughout his career, Jamieson has demonstrated an exceptional ability to navigate the complexities of global markets, leveraging his keen insight and analytical skills to secure profitable deals for his clients.</p>
                             <p>
@@ -36,7 +36,7 @@ const OurTeam = () => {
 
                             </div>
                         </div>
-                        <div className="col-md-6 col-lg-8 col-xl-8 col-sm-12">
+                        <div className="col-md-6 col-lg-8 col-xl-8 col-sm-12 team">
                             <h4>Steve Ahn</h4>
                             <p>A graduate of UC Irvine, Steve has been a strategic and business process consultant since 1999, collaborating with Fortune 500 companies in retail, entertainment, finance, healthcare, public sector, defense, and manufacturing industries. Steve's expertise lies in crafting strategic initiatives and architecting innovative solutions to meet the evolving needs of organizations across diverse sectors.  Steve currently serves as the Chief Strategic Officer and managing director in multiple companies, including software delivery, retail distribution, and finance.</p>
                         </div>
@@ -50,7 +50,7 @@ const OurTeam = () => {
 
                             </div>
                         </div>
-                        <div className="col-md-6 col-lg-8 col-xl-8 col-sm-12">
+                        <div className="col-md-6 col-lg-8 col-xl-8 col-sm-12 team">
                             <h4>Simon Kapita</h4>
                             <p>With over six years at Impel Inc, a leading commodity trading company, I've optimized operations and driven growth through strategic insights and market expertise. Leveraging my proficiency as a certified Scrum Master, I've successfully integrated Agile project management methodologies to enhance efficiency and adaptability within our operations. My ability to navigate complex markets, manage risk, and execute strategic initiatives has bolstered Impel Inc's market presence and profitability. Committed to excellence and innovation, I am dedicated to driving ongoing success in the dynamic realm of commodity trading.</p>
                         </div>
@@ -63,7 +63,7 @@ const OurTeam = () => {
 
                             </div>
                         </div>
-                        <div className="col-md-6 col-lg-8 col-xl-8 col-sm-12">
+                        <div className="col-md-6 col-lg-8 col-xl-8 col-sm-12 team">
                             <h4>Steve Han</h4>
                             <p>STEVE HAN USA, Korea, Africa, Asia, South America, Global Outreach</p>
                             <p>Steve Han founded Trinity Investment in 1999 because he saw the need for a better kind of Brokerage Company. During his 16-year career and foundation in Business law, he has brokered well over 100 billion dollars in real estate transactions, which include office buildings, retail centers, shopping centers, land development, apartment complexes, mobile home parks, mini storages, HUD financed transactions and bond-financed properties.</p>
