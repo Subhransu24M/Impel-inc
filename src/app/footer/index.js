14a03fb3f3e07e30aaa84118f74244c21118eadb
Ebs12345678 <subhransuseo18@gmail.com>
2024-03-21 16:38:15 +0530

    new changes 18032024-20

--- src/app/footer/index.js
+++ src/app/footer/index.js
@@ -4,51 +4,51 @@ import { MdMiscellaneousServices } from "react-icons/md";
 import { GrContactInfo } from "react-icons/gr";
 import Link from 'next/link';
 const Footer = () => {
-    return (
-      <>
+  return (
+    <>
       <h6 className="ftr">© 2024 , Impel Inc</h6>
       <div className="ftr-mbl-mnu-blk">
         <div className="container">
           <div className="row">
             <div className="col-3">
               <div className="ftr-mnu-icon-blk">
-                <Link href="/">
-                <span><IoHomeOutline /></span>
-              <p>Home</p>
+                <Link href="/" className="ftr-lnk">
+                  <span><IoHomeOutline /></span>
+                  <p>Home</p>
                 </Link>
               </div>
-              
+
             </div>
             <div className="col-3">
-            <div className="ftr-mnu-icon-blk">
-              <Link href="/blue-ribbon">
-              <span><MdOutlineProductionQuantityLimits /></span>
-              <p>Products</p>
-              </Link>
+              <div className="ftr-mnu-icon-blk">
+                <Link href="/blue-ribbon" className="ftr-lnk">
+                  <span><MdOutlineProductionQuantityLimits /></span>
+                  <p>Products</p>
+                </Link>
               </div>
             </div>
             <div className="col-3">
-            <div className="ftr-mnu-icon-blk">
-              <Link href="/services">
-              <span><MdMiscellaneousServices /></span>
-              <p>Services</p>
-              </Link>
-              
+              <div className="ftr-mnu-icon-blk">
+                <Link href="/services" className="ftr-lnk">
+                  <span><MdMiscellaneousServices /></span>
+                  <p>Services</p>
+                </Link>
+
               </div>
             </div>
             <div className="col-3">
-            <div className="ftr-mnu-icon-blk">
-              <Link href="/contact-us">
-              <span><GrContactInfo /></span>
-              <p>Inquiry</p>
-              </Link>
+              <div className="ftr-mnu-icon-blk">
+                <Link href="/contact-us" className="ftr-lnk">
+                  <span><GrContactInfo /></span>
+                  <p>Inquiry</p>
+                </Link>
               </div>
             </div>
           </div>
         </div>
       </div>
-      </>
-    )
-  }
-  
-  export default Footer
+    </>
+  )
+}
+
+export default Footer
